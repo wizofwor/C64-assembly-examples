498f26abea3a51836f2cb0631934391d9aed4ed7
wizofwor <ascioglu@yahoo.com>
2018-09-26 00:31:57 +0300

    reorganized

--- tutorials/custom-charset/main.asm
+++ tutorials/custom-charset/main.asm
@@ -1,23 +1,29 @@
-/*=============================================
+/* ------------------------------------------- *
 
-  6502 TUTORIALS - CUSTOM CHARSET 
+  C64 ASM TUTORIALS by wizofwor
 
- ============================================*/
+  CUSTOM CHARSET
+
+  Environment: KickAssembler v.4.12
+  Date: September 2018 
+
+ * ------------------------------------------- */
 
 	.const SCREEN_RAM = $0400
 	.const COLOR_RAM = $d800
 	.const CHARSET = $2000
 
-	BasicUpstart2(start)
+// --- Main Program with Basic Upstarter --------
 
-	*=$c000 "Main Program"
+BasicUpstart2(start)
 
-start:
-// Change Border Color
-	lda #$03
+*=$c000 "Main Program"
+
+	// Change Border Color
+start:	lda #$03
 	sta $d020
 
-// Change Screen Colors
+	// Change Screen Colors
 	ldx #$00
 !:	sta COLOR_RAM,x
 	sta COLOR_RAM+$100,x
@@ -26,14 +32,15 @@ start:
 	inx
 	bne !-
 
-// Change cursor color
+	// Change cursor color
 	sta $286
 
-
+	// Set new charset
 	lda #$18
 	sta $d018
 
 	rts
 	
-	* = CHARSET
-	.import c64 "antik_1.64c"
+// --- Include Data -----------------------------
+* = CHARSET "Character Set"
+.import c64 "antik_1.64c"
